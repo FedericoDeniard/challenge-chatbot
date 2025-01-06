5f3001dee79109023bb2766ee0f13fd09b1fc647
Federico <fededeniard@gmail.com>
2025-01-06 16:47:54 -0300

    .

--- backend/tests/example.test.js
+++ backend/tests/example.test.js
@@ -1,5 +1,5 @@
-import { jest } from "@jest/globals";
-import { sum } from "./funcion.js";
+import { test, expect } from "@jest/globals";
+import { sum } from "./funcion";
 test("nombre del test", () => {
   const result = sum(2, 2);
   expect(result).toBe(4);
